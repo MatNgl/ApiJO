9f089c101c310b1a64ee27e7d6ac20c497dc8903
MatNgl <mattheo.naegellen@gmail.com>
2024-05-29 22:02:09 +0200

    Generation du projet

---
+++ JO-ticket/src/main/java/com/example/JO_ticket/JoTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.JO_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoTicketApplication.class, args);
+	}
+
+}
